edfc144c2b76f72020f197e1b50a7a4ff9ecd6b5
pvc <cuongpv2209@gmail.com>
2021-12-09 18:26:35 +0700

    Xay dung view

---
+++ src/App.java
@@ -0,0 +1,13 @@
+
+import View.ProductView;
+
+
+/**
+ *
+ * @author Pham Van Cuong - KTPM K17A
+ */
+public class App {
+    public static void main(String[] args) {
+        new ProductView();
+    }
+}
